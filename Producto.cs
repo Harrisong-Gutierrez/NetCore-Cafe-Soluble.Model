9d959cfed5119bcd1759bde879dc3bb63fc4e67d
harry-app <harrisonggutierrezgarcia744@gmail.com>
2022-11-17 16:48:28 -0600

    third commit

---
+++ Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCore_Cafe_Soluble.Model
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Contenido { get; set; }
+        public int Peso { get; set; }
+        public string Color { get; set; }
+        public int Cantidad { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
